a1ab482a14fd030f986e1afa1bb5b3c4fd8dc26a
Luciano Bermudez <lucianobmecheng69@gmail.com>
2023-11-04 16:44:51 -0700

    First Project

---
+++ Introduction to C++/Display_UserName.cpp
@@ -0,0 +1,15 @@
+// Learning C++
+// Excersise 1
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string str;
+    cout << "Hello what is your name ?";
+    cin >> str;
+    cout << "Hello , " <<str << " Nice to meet you!" << endl;
+    return (0)
+}
